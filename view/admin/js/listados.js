60b5b49ca6e4e60193dff0977e55e84491b53eea
Pablo <pablogaar01@gmail.com>
2023-01-13 21:41:24 +0100

    cc

--- view/admin/js/listados.js
+++ view/admin/js/listados.js
@@ -165,3 +165,19 @@ function crearCabeceraTabla() {
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
+
+
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        var error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+}
+
+
+function parseJSON(){
+    return response.json();
+}
